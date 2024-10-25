d4384c344365e7a3912b726a52d173100de350a6
josephbill <josephbill00@gmail.com>
2024-10-26 00:50:30 +0300

    add scanner

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ domReady(function () {
 
     // If found you qr code
     function onScanSuccess(decodeText, decodeResult) {
-        alert("You Qr is : " + decodeText, decodeResult);
+        console.log("You Qr is : " + decodeText, decodeResult);
     }
 
     let htmlscanner = new Html5QrcodeScanner(

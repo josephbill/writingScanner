422203a885cb864e5be9890a8ffb2247d1285603
josephbill <josephbill00@gmail.com>
2025-01-10 23:03:57 +0300

    alert to show decoded text

--- script.js
+++ script.js
@@ -76,8 +76,67 @@
                 };
             } else if(orderNumber.toLowerCase()  === 'mpesa'){
                 alert("Mpesa Payment. Ticket Verified")
+                // submitting fulfillment 
+                const data_mpesa = {
+                    "first_name": firstName,
+                    "order_number": orderNumber,
+                    "status" : "fulfilled"
+                  }
+
+ // Making the POST request to the server
+ fetch('http://127.0.0.1:5000/create_ticket_attendance', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' // Tell the server we're sending JSON data
+    },
+    body: JSON.stringify(data_mpesa) // Convert JavaScript object to JSON string
+  })
+  .then(response => response.json()) // Convert the response to JSON
+  .then(result => {
+    if (result.success) {
+      console.log('Mpesa fulfilled.', result);
+      // Do something with the result if needed
+    } else {
+      console.error('Error:', result.message);
+      // Handle error if request fails
+    }
+  })
+  .catch(error => {
+    console.error('Request failed:', error);
+    // Handle network or other errors
+  });                  
             } else if(orderNumber.toLowerCase()  === 'complimentary'){
                 alert("This is a complimentary issued ticket. Ticket Verified")
+
+                const data_complimentary = {
+                    "first_name": firstName,
+                    "order_number": orderNumber,
+                    "status" : "fulfilled"
+                  }
+
+ // Making the POST request to the server
+ fetch('http://127.0.0.1:5000/create_ticket_attendance', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' // Tell the server we're sending JSON data
+    },
+    body: JSON.stringify(data_complimentary) // Convert JavaScript object to JSON string
+  })
+  .then(response => response.json()) // Convert the response to JSON
+  .then(result => {
+    if (result.success) {
+      console.log('Complimentary recorded:', result);
+      // Do something with the result if needed
+    } else {
+      console.error('Error:', result.message);
+      // Handle error if request fails
+    }
+  })
+  .catch(error => {
+    console.error('Request failed:', error);
+    // Handle network or other errors
+  });
+                
             } else {
                 alert("Failed to extract order details from the QR code.");
             }

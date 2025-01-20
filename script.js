bd0575b8c612de77f98cbc4c5bafbb3285f175ab
josephbill <josephbill00@gmail.com>
2025-01-20 15:54:09 +0300

    added auto reloads for sections on success

--- script.js
+++ script.js
@@ -151,12 +151,14 @@ body: JSON.stringify(data_shopify_error) // Convert JavaScript object to JSON st
 if (result.success) {
 // console.log('Shopify order fulfilled. Multiple order fulfillment', result);
 alert('Shopify order fulfilled. Multiple order fulfillment')
+window.location.reload()
 modal.style.display = "none";
 
 // Do something with the result if needed
 } else {
 console.error('Error:', result.message);
 alert(result.message)
+window.location.reload()
 modal.style.display = "none";
 
 // Handle error if request fails
@@ -236,6 +238,7 @@ body: JSON.stringify(data_mpesa) // Convert JavaScript object to JSON string
 if (result.success) {
 console.log('Mpesa fulfilled.', result);
 alert(result.message)
+window.location.reload()
 modal.style.display = "none";
 
 // Do something with the result if needed
@@ -308,6 +311,7 @@ body: JSON.stringify(data_complimentary) // Convert JavaScript object to JSON st
 if (result.success) {
 console.log('Complimentary recorded:', result);
 alert(result.message)
+window.location.reload()
 modal.style.display = "none";
 // Do something with the result if needed
 } else {
